24a4234014f72b8087745b583bf03d2413a2dccd
FireFull45 <firefull2945@gmail.com>
2023-03-22 21:42:00 +0100

    début création sondage

--- src/Controller/PollController.php
+++ src/Controller/PollController.php
@@ -9,6 +9,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\QuestionMCQMultipleType;
+use App\Form\QuestionMCQSingleType;
+use App\Form\QuestionNumberType;
+use App\Form\QuestionTextType;
 
 #[Route('/poll')]
 class PollController extends AbstractController
@@ -24,17 +28,27 @@ class PollController extends AbstractController
     #[Route('/new', name: 'app_poll_new', methods: ['GET', 'POST'])]
     public function new(Request $request, PollRepository $pollRepository): Response
     {
+        $formMultiple = $this->createForm(QuestionMCQMultipleType::class);
+        $formSingle = $this->createForm(QuestionMCQSingleType::class);
+        $formNumber = $this->createForm(QuestionNumberType::class);
+        $formText = $this->createForm(QuestionTextType::class);
+
         $poll = new Poll();
         $form = $this->createForm(PollType::class, $poll);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $poll->setCreationDate(new \DateTime());
             $pollRepository->save($poll, true);
 
             return $this->redirectToRoute('app_poll_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('poll/new.html.twig', [
+            'formMultiple' => $formMultiple->createView(),
+            'formSingle' => $formSingle->createView(),
+            'formNumber' => $formNumber->createView(),
+            'formText' => $formText->createView(),
             'poll' => $poll,
             'form' => $form,
         ]);
@@ -69,7 +83,7 @@ class PollController extends AbstractController
     #[Route('/{id}', name: 'app_poll_delete', methods: ['POST'])]
     public function delete(Request $request, Poll $poll, PollRepository $pollRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$poll->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $poll->getId(), $request->request->get('_token'))) {
             $pollRepository->remove($poll, true);
         }
 
--- src/Form/PollType.php
+++ src/Form/PollType.php
@@ -7,23 +7,62 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PollType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('limitDate')
+            ->add('name', null, [
+                'label' => 'Titre',
+                'attr' => [
+                    'placeholder' => 'Titre du sondage',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Description du sondage',
+                ],
+            ])
+            ->add('limitDate', DateType::class, [
+                'label' => false,
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'yyyy-MM-ddTHH:mm',
+            ])
             ->add('public')
-            ->add('questions', CollectionType::class, [
-                'entry_type' => QuestionType::class,
+            ->add('questionsMCQMultiple', CollectionType::class, [
+                'label' => false,
+                'entry_type' => QuestionMCQMultipleType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
-            ]);
-        ;
+            ])
+            ->add('questionsMCQSingle', CollectionType::class, [
+                'label' => false,
+                'entry_type' => QuestionMCQSingleType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+            ->add('questionsNumber', CollectionType::class, [
+                'label' => false,
+                'entry_type' => QuestionNumberType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+            ->add('questionsText', CollectionType::class, [
+                'label' => false,
+                'entry_type' => QuestionTextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
